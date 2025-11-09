398cf2c314f334aea54688c6e1e56b53b41d5444
nicolasgallu <nicolasgallu@pm.me>
2025-11-09 03:14:49 -0300

    adding logs

--- main.py
+++ main.py
@@ -17,19 +17,17 @@ JOB_FULL_NAME = f"projects/{PROJECT_ID}/locations/{REGION}/jobs/{JOB_NAME}"
 @app.route('/run-scraper', methods=['POST'])
 def run_scraper():
     try:
-        # No need to parse request_json if the job doesn't take args
         
-        # Create a JobExecution request without overrides if no args are needed
+        # Create a JobExecution request
         job_execution_request = run_v2.RunJobRequest(
             name=JOB_FULL_NAME
-            # No 'overrides' needed if your job doesn't take dynamic arguments
         )
 
         logger.info(f"Attempting to execute Cloud Run Job: {JOB_FULL_NAME}")
-        operation = client.run_job(request=job_execution_request)
+        client.run_job(request=job_execution_request)
     
-        logger.info(f"Cloud Run Job execution started: {operation.name}")
-        return jsonify({"status": "Cloud Run Job execution initiated", "operation_name": operation.name}), 202
+        logger.info(f"Cloud Run Job execution started")
+        return jsonify({"status": "Cloud Run Job execution initiated"}), 202
 
     except Exception as e:
         logger.error(f"Error initiating Cloud Run Job: {e}")
